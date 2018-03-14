d5477e518b891843cf8de738b41fb51aa62194e6
kcieslik226138 <kcieslik226138@e-science.pl>
2018-03-15 00:34:17 +0100

    JUnit tests refactoring.

--- src/test/CalculatingComponentsOfTheNextAnniversaryTest.java
+++ src/test/CalculatingComponentsOfTheNextAnniversaryTest.java
@@ -1,7 +1,9 @@
 import org.junit.Test;
 import pl.escience.zdpp.lab03gr1.database.view.ViewExtendedPersonAnniversary;
+
 import java.util.Calendar;
 import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 public class CalculatingComponentsOfTheNextAnniversaryTest {
@@ -29,34 +31,6 @@ public class CalculatingComponentsOfTheNextAnniversaryTest {
         calendarExpectedDate.set(Calendar.YEAR, 2019);
         calendarExpectedDate.set(Calendar.MONTH, 2 - 1);
         calendarExpectedDate.set(Calendar.DAY_OF_MONTH, 21);
-
-        assertEquals(calendarExpectedDate.getTime().getTime(), personAnniversary.getNextAnniversaryDate().getTime());
-    }
-
-    @Test
-    public void calculateDateOfTheNextAnniversary_2(){
-        // Birth date: 25/02/1996.
-        Calendar calendarBirthDate = Calendar.getInstance();
-        calendarBirthDate.set(Calendar.YEAR, 1996);
-        calendarBirthDate.set(Calendar.MONTH, 2 - 1);
-        calendarBirthDate.set(Calendar.DAY_OF_MONTH, 25);
-        Date birthDate = calendarBirthDate.getTime();
-
-        ViewExtendedPersonAnniversary personAnniversary = new ViewExtendedPersonAnniversary();
-        personAnniversary.setAnniversaryDate(birthDate);
-
-        // Today: 21/02/2018.
-        Calendar calendarToday = Calendar.getInstance();
-        calendarToday.set(Calendar.YEAR, 2018);
-        calendarToday.set(Calendar.MONTH, 2 - 1);
-        calendarToday.set(Calendar.DAY_OF_MONTH, 15);
-        personAnniversary.calculateNextAnniversaryFields(calendarToday);
-
-        // Expected date of next anniversary: 25.02.2018.
-        Calendar calendarExpectedDate = Calendar.getInstance();
-        calendarExpectedDate.set(Calendar.YEAR, 2018);
-        calendarExpectedDate.set(Calendar.MONTH, 2 - 1);
-        calendarExpectedDate.set(Calendar.DAY_OF_MONTH, 25);
 
         assertEquals(calendarExpectedDate.getTime().getTime(), personAnniversary.getNextAnniversaryDate().getTime());
     }
